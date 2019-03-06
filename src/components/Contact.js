c79d7caa861f5fbc8994f508c38639300b01674e
DT-1236 <WEMascarinas@gmail.com>
2019-03-05 18:56:30 -0800

    CSS change. Likely to deprecate this page soon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,16 +4,23 @@ import styled from 'styled-components';
 
 const CLink = styled(Link)`
   text-decoration: null;
+  margin: 3px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
 
 class Contact extends Component {
   render() {
     return (
-      <div className="Contact">
+      <Container className="Contact">
         <CLink to="WEMascarinas@gmail.com">WEMascarinas@gmail.com</CLink>
         <CLink to="https://github.com/DT-1236">GitHub</CLink>
         Mobile: (630) 267-8899
-      </div>
+      </Container>
     );
   }
 }
