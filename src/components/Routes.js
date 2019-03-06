6c210d5178ccda2758c3a7cefe1846414163dfb5
DT-1236 <WEMascarinas@gmail.com>
2019-03-05 18:55:51 -0800

    added portfolio page entries

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,16 +3,20 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './HomePage';
 import About from './About';
-// import Portfolio from './Portfolio';
 import Contact from './Contact';
+import Bot from './Bot';
+import Jobly from './Jobly';
+import HOS from './HOS';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/about" render={() => <About />} />
-        {/* <Route exact path="/portfolio" render={() => <Portfolio />} /> */}
         <Route exact path="/contact" render={() => <Contact />} />
+        <Route exact path="/bot" render={() => <Bot />} />
+        <Route exact path="/jobly" render={() => <Jobly />} />
+        <Route exact path="/hos" render={() => <HOS />} />
         <Route render={() => <HomePage />} />
       </Switch>
     );
