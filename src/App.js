ec0a581cfba080a5ddd8ed5fe0a55789d38234d5
DT-1236 <WEMascarinas@gmail.com>
2019-03-05 18:54:31 -0800

    CSS changes

--- src/App.js
+++ src/App.js
@@ -7,20 +7,27 @@ import Routes from './components/Routes';
 const Container = styled.div`
   display: flex;
   justify-content: center;
+  padding-left: 5px;
+  padding-right: 5px;
+
+  @media (min-width: 600px) {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
 `;
 
 const Content = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
   flex-direction: column;
-  padding: 60px 20px;
-  font-weight: bold;
-  border-radius: 20px
-  width: 70%;
   align-self: center;
-  background-color: rebeccapurple;
-  color: lightgray;
+  width: 100vw;
+  height: 100vw;
+  margin-top: 10%;
+
+  @media (min-width: 600px) {
+    width: 80%;
+  }
 `;
 
 class App extends Component {
