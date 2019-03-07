054f65167210475f12b3fec681ece08c7847678f
DT-1236 <WEMascarinas@gmail.com>
2019-03-07 15:18:02 -0800

    Most text laid out

--- src/components/Bot.js
+++ src/components/Bot.js
@@ -57,9 +57,6 @@ class Bot extends Component {
     return (
       <Container className="Bot">
         <Title>Discord Bots</Title>
-        <Technologies>
-          Python, discord.py, requests, fuzzy-wuzzy (fuzzy searching)
-        </Technologies>
         <ImgContainer>
           <Img src={pssLogo} />
         </ImgContainer>
@@ -81,6 +78,15 @@ class Bot extends Component {
           character with slightly inferior stats may end up being superior for a
           role due to available best-in-slot equipment and/or training potential
         </p>
+        <Category>Character Borrowing</Category>
+        <p>
+          Players can lend some of their crew to the alliance pool and allow
+          other members to use them. There was no way to designate an intended
+          recipient. Ship builds are highly specialized, and certain crew
+          members are spectacular in certain roles. If a build is planned with
+          certain lent crew-member in mind, the whole build could fall apart if
+          someone accidentally takes a core crew member
+        </p>
         <Category>Alliance Politics</Category>
         <p>
           End-of-season ladder scoring was based on total trophy count of all
@@ -92,17 +98,65 @@ class Bot extends Component {
 
         <Heading>PSS Bot</Heading>
 
+        <Technologies>
+          Python, discord.py, requests, fuzzy-wuzzy (fuzzy searching)
+        </Technologies>
         <Category>Character / Equipment Information</Category>
 
         <p>
           This bot addressed the character and equipment problems. It did one
           round of API queries on starting, and kept the information in memory.
-          This information could be refreshed with the correct user input. From
-          the comfort of the Discord channel, a player could query a character
-          and get information regarding all prestige combinations using or
-          resulting in that character. It would also give base stats as well as
-          all possible stats given the current best-in-slot gear and a
-          reasonable amount of training
+          This information could be refreshed with the correct user input.
+        </p>
+        <ImgContainer>{/* <Img src={characterInfo} /> */}</ImgContainer>
+
+        <p>
+          From the comfort of the Discord channel, a player could query a
+          character and get information regarding all prestige combinations
+          using or resulting in that character. It would also give base stats as
+          well as all possible stats given the current best-in-slot gear and a
+          reasonable amount of training. If two characters were given, it would
+          offer a comparison of base stats and best stats with best-in-slot gear
+        </p>
+
+        <Heading>Council Bot</Heading>
+        <Technologies>
+          Python, discord.py, requests, fuzzy-wuzzy, MS SQL, pyodbc, Matplotlib
+        </Technologies>
+
+        <Category>Alliance / Trophy Information</Category>
+        <p>
+          Council bot helped in making decisions around alliance
+          metrics/politics. It queried the PSS API for the current tropy count
+          of every ship in every alliance in the top 50. At most it would do so
+          once a day. Information was stored in an MS SQL database. Queries
+          would be constructed based on inputs from the Discord channel.
+        </p>
+        <ImgContainer>{/* <Img src={pssTrophyCount} /> */}</ImgContainer>
+
+        <p>
+          Again, final scoring was based off of verbal agreements between
+          players to join specific alliances at the end of the season.
+          Frequently, such agreements were not honored or changed to serve
+          various interests. Council Bot was able to provide a list of all known
+          alliance affiliations to assist leadership in calculations and
+          decisions. It also helped identify high-profile players by reporting a
+          player's trophy count over time
+        </p>
+
+        <Category>Crew Recipient</Category>
+        <p>
+          Council Bot could take a given ShipID and identify the receiving
+          player for loaned crew members. It did so by querying the PSS API for
+          every ship in the alliance, looking through their crew, and matching
+          the originating ID to the supplied ID.{' '}
+        </p>
+
+        <ImgContainer>{/* <Img src={pssCrewRecipient} /> */}</ImgContainer>
+
+        <p>
+          This helped leadership try to correct errant behavior either by verbal
+          advice or outright ejection
         </p>
         <p>Under construction</p>
         <p>
